c80d2ff40284cc698227b9ec900b1e2bc22ec1e0
Joseph Tafese <jetafese@uwaterloo.ca>
2021-09-22 17:41:04 -0400

    change to btor namespace

---
+++ include/Btor/BtorDialect.h
@@ -0,0 +1,16 @@
+//===- BtorDialect.h - Btor dialect -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef BTOR_BTORDIALECT_H
+#define BTOR_BTORDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "Btor/BtorOpsDialect.h.inc"
+
+#endif // BTOR_BTORDIALECT_H
