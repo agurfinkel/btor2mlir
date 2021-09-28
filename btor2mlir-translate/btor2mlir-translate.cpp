733bdf0f4a5fc6188a57af111bb5bd9c465d7543
Joseph Tafese <jetafese@uwaterloo.ca>
2021-09-28 12:44:37 -0400

    chore: match llvm project structure

--- btor2mlir-translate/btor2mlir-translate.cpp
+++ btor2mlir-translate/btor2mlir-translate.cpp
@@ -15,7 +15,7 @@
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Translation.h"
 
-#include "Btor/BtorDialect.h"
+#include "Dialect/Btor/IR/BtorDialect.h"
 
 int main(int argc, char **argv) {
   mlir::registerAllTranslations();
