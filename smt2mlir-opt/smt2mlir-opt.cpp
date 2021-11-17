a08b3d1021496a2cd6cbc3594e38f9ebd578adc1
Joseph Tafese <jetafese@uwaterloo.ca>
2021-11-16 23:27:27 -0500

    feat: add opt and translate drivers

---
+++ smt2mlir-opt/smt2mlir-opt.cpp
@@ -0,0 +1,34 @@
+//===- smt2mlir-opt.cpp ---------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Support/MlirOptMain.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+#include "Dialect/Smt/IR/SmtDialect.h"
+#include "Conversion/Passes.h"
+
+int main(int argc, char **argv) {
+  mlir::registerAllPasses();
+
+  mlir::DialectRegistry registry;
+  registry.insert<mlir::smt::SmtDialect>();
+  registry.insert<mlir::StandardOpsDialect>();
+
+  return mlir::asMainReturnCode(
+      mlir::MlirOptMain(argc, argv, "Smt optimizer driver\n", registry));
+}
