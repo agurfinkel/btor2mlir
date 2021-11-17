a3d18dacf1b046e7d93f48a94a26fa9be86bf16f
Joseph Tafese <jetafese@uwaterloo.ca>
2021-11-16 23:26:49 -0500

    feat: add smt lib files

---
+++ lib/Dialect/Smt/IR/SmtDialect.cpp
@@ -0,0 +1,29 @@
+//===- SmtDialect.cpp - Smt dialect ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Dialect/Smt/IR/SmtDialect.h"
+#include "Dialect/Smt/IR/SmtOps.h"
+
+#include "Dialect/Smt/IR/SmtOpsDialect.cpp.inc"
+
+// Pull in all enum type definitions and utility function declarations.
+#include "Dialect/Smt/IR/SmtOpsEnums.cpp.inc"
+
+using namespace mlir;
+using namespace mlir::smt;
+
+//===----------------------------------------------------------------------===//
+// Smt dialect.
+//===----------------------------------------------------------------------===//
+
+void SmtDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "Dialect/Smt/IR/SmtOps.cpp.inc"
+      >();
+}
---
+++ lib/Dialect/Smt/IR/SmtOps.cpp
@@ -0,0 +1,13 @@
+//===- SmtOps.cpp - Smt dialect ops ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Dialect/Smt/IR/SmtDialect.h"
+#include "Dialect/Smt/IR/SmtOps.h"
+
+#define GET_OP_CLASSES
+#include "Dialect/Smt/IR/SmtOps.cpp.inc"
