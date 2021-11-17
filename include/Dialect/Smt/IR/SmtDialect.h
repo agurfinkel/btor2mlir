033bd829d0fe4b85cea249284bc352ba2f97df19
Joseph Tafese <jetafese@uwaterloo.ca>
2021-11-16 23:24:57 -0500

    feat: add smt dialect headers;

---
+++ include/Dialect/Smt/IR/SmtDialect.h
@@ -0,0 +1,16 @@
+//===- SmtDialect.h - Smt dialect -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SMT_SMTDIALECT_H
+#define SMT_SMTDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "Dialect/Smt/IR/SmtOpsDialect.h.inc"
+
+#endif // SMT_SMTDIALECT_H
