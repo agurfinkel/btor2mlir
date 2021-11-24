d604425304611c3afe352dcef88646e305c4db9e
Joseph Tafese <jetafese@uwaterloo.ca>
2021-11-24 12:22:04 -0500

    fix: wrap access to m_cache

--- lib/Target/Btor/BtorToBtorIRTranslation.cpp
+++ lib/Target/Btor/BtorToBtorIRTranslation.cpp
@@ -79,7 +79,8 @@ bool Deserialize::parseModelIsSuccessful() {
 ///===----------------------------------------------------------------------===//
 /// This function's goal is to create the MLIR Operation that corresponds to 
 /// the given Btor2Line*, cur, into the basic block designated by the class 
-/// field m_builder.
+/// field m_builder. Make sure that the kids have already been created before 
+/// calling this method
 ///
 /// e.x:
 ///     Operation * res = createMLIR(cur, cur->args);
@@ -92,157 +93,157 @@ Operation * Deserialize::createMLIR(const Btor2Line *line, const int64_t *kids) 
   // binary ops
   case BTOR2_TAG_slt:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::slt, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_slte:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::sle, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sgt:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::sgt, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sgte:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::sge, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_neq:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::ne, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_eq:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::eq, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_ugt:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::ugt, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_ugte:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::uge, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_ult:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::ult, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_ulte:
     res = buildComparisonOp<btor::CmpOp>(btor::BtorPredicate::ule, 
-                            cache.at(kids[0]), cache.at(kids[1]));
+                            m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   
   case BTOR2_TAG_concat:
-    res = buildConcatOp(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildConcatOp(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_add:
-    res = buildBinaryOp<btor::AddOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::AddOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_and:
-    res = buildBinaryOp<btor::AndOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::AndOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_implies:
-    res = buildBinaryOp<btor::ImpliesOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::ImpliesOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_iff:
-    res = buildBinaryOp<btor::IffOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::IffOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_nand:
-    res = buildBinaryOp<btor::NandOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::NandOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_nor:
-    res = buildBinaryOp<btor::NorOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::NorOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_or:
-    res = buildBinaryOp<btor::OrOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::OrOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sdiv:
-    res = buildBinaryOp<btor::SDivOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::SDivOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_srem:
-    res = buildBinaryOp<btor::SRemOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::SRemOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sub:
-    res = buildBinaryOp<btor::SubOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::SubOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_udiv:
-    res = buildBinaryOp<btor::UDivOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::UDivOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_urem:
-    res = buildBinaryOp<btor::URemOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::URemOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_mul:
-    res = buildBinaryOp<btor::MulOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::MulOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_smod:
-    res = buildBinaryOp<btor::SModOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::SModOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_xnor:
-    res = buildBinaryOp<btor::XnorOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::XnorOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_xor:
-    res = buildBinaryOp<btor::XOrOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::XOrOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sll:
-    res = buildBinaryOp<btor::ShiftLLOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::ShiftLLOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sra:
-    res = buildBinaryOp<btor::ShiftRAOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::ShiftRAOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_srl:
-    res = buildBinaryOp<btor::ShiftRLOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::ShiftRLOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_rol:
-    res = buildBinaryOp<btor::RotateLOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::RotateLOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_ror:
-    res = buildBinaryOp<btor::RotateROp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildBinaryOp<btor::RotateROp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_saddo:
-    res = buildOverflowOp<btor::SAddOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::SAddOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_sdivo:
-    res = buildOverflowOp<btor::SDivOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::SDivOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_smulo:
-    res = buildOverflowOp<btor::SMulOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::SMulOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_ssubo:
-    res = buildOverflowOp<btor::SSubOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::SSubOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_uaddo:
-    res = buildOverflowOp<btor::UAddOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::UAddOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_umulo:
-    res = buildOverflowOp<btor::UMulOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::UMulOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
   case BTOR2_TAG_usubo:
-    res = buildOverflowOp<btor::USubOverflowOp>(cache.at(kids[0]), cache.at(kids[1]));
+    res = buildOverflowOp<btor::USubOverflowOp>(m_cache.at(kids[0]), m_cache.at(kids[1]));
     break;
 
   // unary ops
   case BTOR2_TAG_dec:
-    res = m_builder.create<btor::DecOp>(m_unknownLoc, cache.at(kids[0]));
+    res = m_builder.create<btor::DecOp>(m_unknownLoc, m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_inc:
-    res = m_builder.create<btor::IncOp>(m_unknownLoc, cache.at(kids[0]));
+    res = m_builder.create<btor::IncOp>(m_unknownLoc, m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_neg:
-    res = m_builder.create<btor::NegOp>(m_unknownLoc, cache.at(kids[0]));
+    res = m_builder.create<btor::NegOp>(m_unknownLoc, m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_not:
-    res = m_builder.create<btor::NotOp>(m_unknownLoc, cache.at(kids[0]));
+    res = m_builder.create<btor::NotOp>(m_unknownLoc, m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_redand:
     res = m_builder.create<btor::RedAndOp>(m_unknownLoc, m_builder.getIntegerType(1),
-                                         cache.at(kids[0]));
+                                         m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_redor:
     res = m_builder.create<btor::RedOrOp>(m_unknownLoc, m_builder.getIntegerType(1),
-                                        cache.at(kids[0]));
+                                        m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_redxor:
     res = m_builder.create<btor::RedXorOp>(m_unknownLoc, m_builder.getIntegerType(1),
-                                         cache.at(kids[0]));
+                                         m_cache.at(kids[0]));
     break;
   case BTOR2_TAG_const:
     res = buildConstantOp(line->sort.bitvec.width,
@@ -269,28 +270,28 @@ Operation * Deserialize::createMLIR(const Btor2Line *line, const int64_t *kids) 
                         std::string("zero"), 10);
     break;
   case BTOR2_TAG_bad:
-    res = m_builder.create<btor::AssertNotOp>(m_unknownLoc, cache.at(kids[0]));
+    res = m_builder.create<btor::AssertNotOp>(m_unknownLoc, m_cache.at(kids[0]));
     break;
 
   // indexed ops
   case BTOR2_TAG_slice:
-    res = buildSliceOp(cache.at(kids[0]), kids[1], kids[2]);
+    res = buildSliceOp(m_cache.at(kids[0]), kids[1], kids[2]);
     break;
   case BTOR2_TAG_sext:
     res = m_builder.create<btor::SExtOp>(
-        m_unknownLoc, cache.at(kids[0]),
+        m_unknownLoc, m_cache.at(kids[0]),
         m_builder.getIntegerType(line->sort.bitvec.width));
     break;
   case BTOR2_TAG_uext:
     res = m_builder.create<btor::UExtOp>(
-        m_unknownLoc, cache.at(kids[0]),
+        m_unknownLoc, m_cache.at(kids[0]),
         m_builder.getIntegerType(line->sort.bitvec.width));
     break;
 
   // ternary ops
   case BTOR2_TAG_ite:
-    res = m_builder.create<btor::IteOp>(m_unknownLoc, cache.at(kids[0]),
-                                      cache.at(kids[1]), cache.at(kids[2]));
+    res = m_builder.create<btor::IteOp>(m_unknownLoc, m_cache.at(kids[0]),
+                                      m_cache.at(kids[1]), m_cache.at(kids[2]));
     break;
 
   // unmapped ops
@@ -317,10 +318,11 @@ Operation * Deserialize::createMLIR(const Btor2Line *line, const int64_t *kids) 
 /// the cache, only after the caller ensures that the original line has been 
 /// created and saved in the cache
 ///===----------------------------------------------------------------------===//
-void Deserialize::createNegateLine(int64_t curAt, const Value &child) {
-  auto res = m_builder.create<btor::NotOp>(m_unknownLoc, cache.at(curAt * -1));
+void Deserialize::createNegateLine(int64_t negativeLine, const Value &child) {
+  Value originalLine = getFromCacheById(negativeLine * -1);
+  auto res = m_builder.create<btor::NotOp>(m_unknownLoc, originalLine);
   assert(res && res->getNumResults() == 1);
-  cache[curAt] = res->getResult(0);
+  setCacheWithId(negativeLine, res->getResult(0));
 }
 
 ///===----------------------------------------------------------------------===//
@@ -364,7 +366,7 @@ bool Deserialize::isValidChild(Btor2Line * line) {
 ///  the prerequisite operations before storing the result in our cache
 ///===----------------------------------------------------------------------===//
 void Deserialize::toOp(Btor2Line *line) {
-  if (cache.find(line->id) != cache.end()) {
+  if (valueAtIdIsInCache(line->id)) {
     return;
   }
 
@@ -373,18 +375,19 @@ void Deserialize::toOp(Btor2Line *line) {
   todo.push_back(line);
   while (!todo.empty()) {
     auto cur = todo.back();
-    uint32_t oldsize = todo.size();
+    unsigned oldsize = todo.size();
     for (uint32_t i = 0; i < cur->nargs; ++i) {
       auto arg_i = cur->args[i];
+      // exit early if we do not need to compute this line
       if (arg_i > 0 && !isValidChild(getLineById(arg_i))) {
         continue;
       }
-
-      if (cache.find(arg_i) == cache.end()) {
+      // only look at uncomputed lines
+      if (!valueAtIdIsInCache(arg_i)) {
         if (arg_i < 0) {
-          // if original operation is cached, negate it
-          if (cache.find(arg_i * -1) != cache.end()) {
-            createNegateLine(arg_i, cache.at(arg_i * -1)); 
+          // if original operation is cached, negate it on the fly
+          if (valueAtIdIsInCache(arg_i * -1)) {
+            createNegateLine(arg_i, getFromCacheById(arg_i * -1)); 
           } else {
             todo.push_back(getLineById(arg_i * -1));
           }
@@ -397,13 +400,13 @@ void Deserialize::toOp(Btor2Line *line) {
       continue;
     }
     if (!isValidChild(cur) 
-    || cache.find(cur->id) != cache.end()) {
+    || valueAtIdIsInCache(cur->id)) {
       todo.pop_back();
       continue;
     }
     res = createMLIR(cur, cur->args);
     assert(res && res->getNumResults() < 2 && res->getResult(0));
-    cache[cur->id] = res->getResult(0);
+    setCacheWithId(cur->id, res->getResult(0));
     todo.pop_back();
   }
 }
@@ -429,7 +432,7 @@ OwningOpRef<FuncOp> Deserialize::buildInitFunction() {
   m_builder.setInsertionPointToStart(body);
 
   // clear cache so that values are mapped to the right Basic Block
-  cache.clear();
+  m_cache.clear();
   for (auto init : m_inits) { toOp(init); }
 
   // close with a fitting returnOp
@@ -440,7 +443,7 @@ OwningOpRef<FuncOp> Deserialize::buildInitFunction() {
     if (m_states.at(i)->init > 0) {
       // get the result of init's second argument since
       // that is what we assign our state to  
-      testResults[i] = cache.at(m_inits.at(j++)->args[1]);
+      testResults[i] = getFromCacheById(m_inits.at(j++)->args[1]);
     } else {
       auto sort = returnTypes.at(i).getIntOrFloatBitWidth();
       if (undefOpsBySort.find(sort) == undefOpsBySort.end()) {
@@ -478,10 +481,11 @@ OwningOpRef<FuncOp> Deserialize::buildNextFunction() {
   m_builder.setInsertionPointToStart(body);
 
   // clear cache so that values are mapped to the right Basic Block
-  cache.clear();
+  m_cache.clear();
   // initialize states with block arguments
-  for (uint32_t i = 0; i < m_nexts.size(); ++i) {
-    cache[m_nexts.at(i)->args[0]] = body->getArguments()[i];
+  for (uint32_t i = 0; i < m_states.size(); ++i) {
+    auto stateId = m_states.at(i)->id;
+    setCacheWithId(stateId, body->getArguments()[i]);
   }
 
   // start with nexts, then add bads, for logic sharing
@@ -491,10 +495,8 @@ OwningOpRef<FuncOp> Deserialize::buildNextFunction() {
   // close with a fitting returnOp
   std::vector<Value> testResults(m_nexts.size(), nullptr);
   for (uint32_t i = 0; i < m_nexts.size(); ++i) {
-    testResults[i] = cache.at(m_nexts.at(i)->args[1]);
-    assert(testResults[i]);
+    testResults[i] = getFromCacheById(m_nexts.at(i)->args[1]);
   }
-
   m_builder.create<ReturnOp>(m_unknownLoc, testResults);
   return funcOp;
 }
