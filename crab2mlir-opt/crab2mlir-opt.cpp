076883a8d110adbedfa9acb3bdf706ed6d731f90
Joseph Tafese <jetafese@uwaterloo.ca>
2021-12-07 20:45:58 -0500

    feat: driver code for crab ir

---
+++ crab2mlir-opt/crab2mlir-opt.cpp
@@ -0,0 +1,34 @@
+//===- crab2mlir-opt.cpp ---------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Support/MlirOptMain.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+#include "Dialect/Crab/IR/CrabDialect.h"
+#include "Conversion/Passes.h"
+
+int main(int argc, char **argv) {
+  mlir::registerAllPasses();
+
+  mlir::DialectRegistry registry;
+  registry.insert<mlir::crab::CrabDialect>();
+  registry.insert<mlir::StandardOpsDialect>();
+
+  return mlir::asMainReturnCode(
+      mlir::MlirOptMain(argc, argv, "Crab optimizer driver\n", registry));
+}
