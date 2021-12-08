f2a948fe8310363494c152408ccd703ed75695ab
Joseph Tafese <jetafese@uwaterloo.ca>
2021-12-08 00:15:52 -0500

    feat: add constant ops

--- lib/Dialect/Crab/IR/CrabOps.cpp
+++ lib/Dialect/Crab/IR/CrabOps.cpp
@@ -8,6 +8,37 @@
 
 #include "Dialect/Crab/IR/CrabDialect.h"
 #include "Dialect/Crab/IR/CrabOps.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/Builders.h"
+
+using namespace mlir;
+using namespace mlir::crab;
+
+//===----------------------------------------------------------------------===//
+// ConstantOp
+//===----------------------------------------------------------------------===//
+
+static void printConstantOp(OpAsmPrinter &p, mlir::crab::ConstantOp &op) {
+  p << " ";
+  p.printOptionalAttrDict(op->getAttrs(), /*elidedAttrs=*/{"value"});
+  p << op.getValue();
+}
+
+static ParseResult parseConstantOp(OpAsmParser &parser,
+                                   OperationState &result) {
+  Attribute valueAttr;
+  if (parser.parseOptionalAttrDict(result.attributes) ||
+      parser.parseAttribute(valueAttr, "value", result.attributes))
+    return failure();
+
+  // Add the attribute type to the list.
+  return parser.addTypeToList(valueAttr.getType(), result.types);
+}
+
+OpFoldResult ConstantOp::fold(ArrayRef<Attribute> operands) {
+  assert(operands.empty() && "constant has no operands");
+  return getValue();
+}
 
 #define GET_OP_CLASSES
 #include "Dialect/Crab/IR/CrabOps.cpp.inc"
