fd19b83d78d4706355746d3f83bd226831d83c65
Joseph Tafese <jetafese@uwaterloo.ca>
2021-12-07 20:46:53 -0500

    feat: add lib files for crab ir

---
+++ lib/Dialect/Crab/IR/CrabOps.cpp
@@ -0,0 +1,13 @@
+//===- CrabOps.cpp - Crab dialect ops ---------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Dialect/Crab/IR/CrabDialect.h"
+#include "Dialect/Crab/IR/CrabOps.h"
+
+#define GET_OP_CLASSES
+#include "Dialect/Crab/IR/CrabOps.cpp.inc"
