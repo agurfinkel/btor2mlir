5c218355e7ba5ac06ac0aa7d5d834bbe6fe2df04
Joseph Tafese <jetafese@uwaterloo.ca>
2021-12-15 20:16:00 -0500

    feat: add straigtfoward array ops conversion

--- lib/Conversion/BtorToMemRef/BtorToMemRef.cpp
+++ lib/Conversion/BtorToMemRef/BtorToMemRef.cpp
@@ -2,9 +2,10 @@
 #include "Dialect/Btor/IR/BtorOps.h"
 
 #include "../PassDetail.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Analysis/Liveness.h"
 
 using namespace mlir;
 using namespace mlir::btor;
@@ -12,18 +13,36 @@ using namespace mlir::btor;
 #define PASS_NAME "convert-btor-to-memref"
 
 namespace {
-struct BtorToMemRefLoweringPass : public PassWrapper<BtorToMemRefLoweringPass, OperationPass<ModuleOp>> {
-    void getDependentDialects(DialectRegistry &registry) const override {
-        registry.insert<memref::MemRefDialect>();
-    }
-    StringRef getArgument() const final { return PASS_NAME; }
-    void runOnOperation() override;
+struct BtorToMemRefLoweringPass
+    : public PassWrapper<BtorToMemRefLoweringPass, OperationPass<ModuleOp>> {
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<memref::MemRefDialect>();
+  }
+  StringRef getArgument() const final { return PASS_NAME; }
+  void runOnOperation() override;
 };
 
 //===----------------------------------------------------------------------===//
 // Lowering Declarations
 //===----------------------------------------------------------------------===//
 
+struct ArrayOpLowering : public OpRewritePattern<btor::ArrayOp> {
+  using OpRewritePattern<mlir::btor::ArrayOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(btor::ArrayOp op, 
+                                PatternRewriter &rewriter) const override;
+};
+
+struct ReadOpLowering : public OpRewritePattern<btor::ReadOp> {
+  using OpRewritePattern<mlir::btor::ReadOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(btor::ReadOp op, 
+                                PatternRewriter &rewriter) const override;
+};
+
+struct WriteOpLowering : public OpRewritePattern<btor::WriteOp> {
+  using OpRewritePattern<mlir::btor::WriteOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(btor::WriteOp op, 
+                                PatternRewriter &rewriter) const override;
+};
 
 } // end anonymous namespace
 
@@ -31,26 +50,60 @@ struct BtorToMemRefLoweringPass : public PassWrapper<BtorToMemRefLoweringPass, O
 // Lowering Definitions
 //===----------------------------------------------------------------------===//
 
+LogicalResult
+ArrayOpLowering::matchAndRewrite(btor::ArrayOp op, 
+                                 PatternRewriter &rewriter) const {
+  Type type = op.getType();
+  MemRefType memrefType = type.cast<MemRefType>();
+  rewriter.replaceOpWithNewOp<memref::AllocOp>(op, memrefType);
+  return success();
+}
 
+LogicalResult
+ReadOpLowering::matchAndRewrite(btor::ReadOp op, 
+                                 PatternRewriter &rewriter) const {
+  rewriter.replaceOpWithNewOp<memref::LoadOp>(op, op.array(), op.index()); 
+  return success();
+}
+
+LogicalResult
+WriteOpLowering::matchAndRewrite(btor::WriteOp op, 
+                                 PatternRewriter &rewriter) const {
+  Type type = op.getType();
+  Location loc = op.getLoc();
+  MemRefType memrefType = type.cast<MemRefType>();
+  auto newArray = rewriter.create<memref::AllocOp>(loc, memrefType);
+  rewriter.create<memref::CopyOp>(loc, op.array(), newArray);
+  rewriter.create<memref::StoreOp>(loc, op.value(), newArray, op.index());
+  rewriter.replaceOpWithNewOp<memref::AllocOp>(op, memrefType);
+  return success();
+}
 //===----------------------------------------------------------------------===//
 // Populate Lowering Patterns
 //===----------------------------------------------------------------------===//
 
-void mlir::btor::populateBtorToMemRefConversionPatterns(RewritePatternSet &patterns) {
-//   patterns.add<AddLowering, MulLowering, AndLowering>(patterns.getContext());
+void mlir::btor::populateBtorToMemRefConversionPatterns(
+  RewritePatternSet &patterns) {
+  patterns.add<
+    ArrayOpLowering,
+    ReadOpLowering, 
+    WriteOpLowering
+  >(patterns.getContext());
 }
 
 void BtorToMemRefLoweringPass::runOnOperation() {
-    RewritePatternSet patterns(&getContext());
-    populateBtorToMemRefConversionPatterns(patterns);
-    ConversionTarget target(getContext());
-    // target.addIllegalOp<mlir::btor::AddOp, mlir::btor::MulOp, mlir::btor::AndOp>();
-    target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
-    if (failed(applyPartialConversion(getOperation(), target, std::move(patterns)))) {
-        signalPassFailure();
-    }
+  RewritePatternSet patterns(&getContext());
+  populateBtorToMemRefConversionPatterns(patterns);
+  ConversionTarget target(getContext());
+  target.addIllegalOp<btor::ArrayOp, btor::ReadOp, btor::WriteOp>();
+  // >();
+  target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
+  if (failed(applyPartialConversion(getOperation(), target,
+                                    std::move(patterns)))) {
+    signalPassFailure();
+  }
 }
 
 std::unique_ptr<mlir::Pass> mlir::btor::createLowerToMemRefPass() {
-    return std::make_unique<BtorToMemRefLoweringPass>(); 
+  return std::make_unique<BtorToMemRefLoweringPass>();
 }
